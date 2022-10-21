fa779e2ad277d4c3fd61b36a76b047614139d02f
Senay Gebrat <senaygebrat@gmail.com>
2022-10-20 18:25:35 -0700

    initiatized readme

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs');
+const { default: inquirer } = require('inquirer');
+const questions = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+questions.prompt([
+  {
+    type: 'input',
+    name: 'Project',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'Description',
+    message: 'Please provide a description of your README',
+  },
+  {
+    type: 'checkbox',
+    message: 'Would you like a Table of Contents?',
+    name: 'stack',
+    choices: ['Installation', 'Usage', 'Credits', 'License'],
+  },
+  {
+    type: 'list',
+    message: 'What is your preferred method of communication?',
+    name: 'contact',
+    choices: ['email', 'phone', 'telekinesis'],
+  },
+])
+
+
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+
+//function to initialize app
+function init() {
+  inquirer.prompt(questions)
+  .then((response) => {
+    fs.writeFile('./generated-readme.md', generateMarkdown(response))
+  }
+)
+}
 
 // Function call to initialize app
 init();
+
+
+
